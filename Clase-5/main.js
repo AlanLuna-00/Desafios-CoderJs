547abbb8827f3517ce8a2ed0dda778adc7cbfb42
AlanLuna-00 <alan.luna.desarrollo@gmail.com>
2021-09-16 01:14:20 -0300

    Desafio 5.1

--- Clase-5/main.js
+++ Clase-5/main.js
@@ -1,25 +1,28 @@
-class zapato {
+class Zapato {
     constructor(articulo, color, precio, cantidad) {
-        this.articulo = articulo;
-        this.color = color;
+        this.articulo = articulo.toLowerCase();
+        this.color = color.toLowerCase();
         this.precio = precio;
         this.cantidad = cantidad;
     }
     toString() {
         `Zapato articulo: ${this.articulo}\nColor del Zapato: ${this.color}\nPrecio: ${this.precio}\nCantidad de pares ${this.cantidad}`
     }
+    precioTotal() {
+        let sumaTotal = precio * cantidad
+        alert(`El precio final es de ${sumaTotal}`)
+    }
 }
 
 const articulo = prompt('Ingrese el nombre del articulo que desea comprar')
 const color = prompt('Ingrese el color del zapato')
-const precio = Number(prompt('Ingrese el precio del zapato en vidriera'))
-const cantidad = Number(prompt('Ingrese la cantidad de pares que desea llevar'))
+const precio = parseInt(prompt('Ingrese el precio del zapato en vidriera'))
+const cantidad = parseInt(prompt('Ingrese la cantidad de pares que desea llevar'))
 
-const zapato1 = new zapato (articulo, color, precio, cantidad)
+const zapato1 = new Zapato (articulo, color, precio, cantidad)
 
 console.log(zapato1)
+zapato1.precioTotal()
 
-// const precioFinal = (a, b) => {a * b}
-let precioTotal = (cantidad * precio)
 
-alert(`El precio final es de ${precioTotal}`)
+
