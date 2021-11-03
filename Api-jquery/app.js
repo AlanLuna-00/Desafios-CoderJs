e60e2cbeded94b455577258c6f1efcc7cd18460c
AlanLuna-00 <alan.luna.desarrollo@gmail.com>
2021-11-03 00:14:01 -0300

    ajax jquery

---
+++ Api-jquery/app.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+    $.ajax({
+        type: "GET",
+        url: 'https://rickandmortyapi.com/api/character',
+        dataType: 'json',
+        success : function(data) {
+            console.log(data);
+            for (let i = 0; i < data.results.length; i++) {
+                $('.container').append(`
+                    <div class="card col-3 shadow">
+                        <img src="${data.results[i].image}" class="rounded" alt="${data.results[i].name}">
+                        <div class="card-body">
+                            <h5 class="card-title">Nombre: ${data.results[i].name}</h5>
+                            <p class="card-text">Estado: ${data.results[i].status}</p>
+                            <p class="card-text">Especie: ${data.results[i].species}</p>
+                        </div>
+                    </div>
+                `);
+            }
+        }
+    })
+})
+    
+
+
+
+
+    
